82bb56e5b166562e479404cf9a22e87fb10358e8
TakaraMoriwaki <takara.moriwaki@stunited.vn>
2023-09-13 17:41:04 +0700

    Add: gallery content, style, script

--- script.js
+++ script.js
@@ -5,3 +5,31 @@ function openSideMenu(){
 function closeSideMenu(){
   document.getElementById("right-side-menu").style.transform = "translateX(100%)";
 }
+
+const modalImg = document.getElementById("modal-img");
+
+function openModal(num){
+  modalImg.src = "https://devplus.edu.vn/assets/images/devplus/"+num+".png";
+
+  document.getElementById("modal").classList.add("modal-active");
+}
+
+function closeModal(){
+  document.getElementById("modal").classList.remove("modal-active");
+}
+
+function showPreviousImg(){
+  const src = modalImg.src;
+  const currentNum = parseInt(src.charAt(src.length - 5));
+  const nextNum = currentNum != 1 ? currentNum - 1 : 6;
+  console.log(nextNum);
+  modalImg.src = "https://devplus.edu.vn/assets/images/devplus/"+nextNum+".png";
+}
+
+function showNextImg(){
+  const src = modalImg.src;
+  const currentNum = parseInt(src.charAt(src.length - 5));
+  const nextNum = currentNum != 6 ? currentNum + 1 : 1;
+  console.log(nextNum);
+  modalImg.src = "https://devplus.edu.vn/assets/images/devplus/"+nextNum+".png";
+}
