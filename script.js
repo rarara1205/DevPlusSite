2ec705102920ea63f580f74d8c3b7d141b702f0e
TakaraMoriwaki <takara.moriwaki@stunited.vn>
2023-09-13 10:52:06 +0700

    Add: side menu open, close

---
+++ script.js
@@ -0,0 +1,7 @@
+function openSideMenu(){
+  document.getElementById("right-side-menu").style.transform = "translateX(0)";
+}
+
+function closeSideMenu(){
+  document.getElementById("right-side-menu").style.transform = "translateX(100%)";
+}
