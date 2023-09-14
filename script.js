1a80396ce0648d1abfcde40c59f1dd391f2723f1
TakaraMoriwaki <takara.moriwaki@stunited.vn>
2023-09-14 13:54:24 +0700

    Add: qa accordion

--- script.js
+++ script.js
@@ -10,7 +10,6 @@ const modalImg = document.getElementById("modal-img");
 
 function openModal(num){
   modalImg.src = "https://devplus.edu.vn/assets/images/devplus/"+num+".png";
-
   document.getElementById("modal").classList.add("modal-active");
 }
 
@@ -22,7 +21,6 @@ function showPreviousImg(){
   const src = modalImg.src;
   const currentNum = parseInt(src.charAt(src.length - 5));
   const nextNum = currentNum != 1 ? currentNum - 1 : 6;
-  console.log(nextNum);
   modalImg.src = "https://devplus.edu.vn/assets/images/devplus/"+nextNum+".png";
 }
 
@@ -30,6 +28,39 @@ function showNextImg(){
   const src = modalImg.src;
   const currentNum = parseInt(src.charAt(src.length - 5));
   const nextNum = currentNum != 6 ? currentNum + 1 : 1;
-  console.log(nextNum);
   modalImg.src = "https://devplus.edu.vn/assets/images/devplus/"+nextNum+".png";
+}
+
+let currentAnsNum = undefined;
+let currentActiveBtn = undefined;
+
+const btns = document.getElementsByClassName("accordion-btn");
+const bodys = document.getElementsByClassName("accordion-body");
+for(i=0;i<btns.length;i++){
+  btns[i].addEventListener("click",showAnswer);
+}
+
+function showAnswer(e){
+  const qa = document.getElementById("qa-1");
+  if(currentAnsNum == undefined){
+    console.log("first");
+    currentAnsNum = e.target.value-1;
+    btns[currentAnsNum].classList.add("active");
+    bodys[currentAnsNum].classList.add("active");
+    bodys[currentAnsNum].style.setProperty("--max-height", `${bodys[currentAnsNum].firstElementChild.getBoundingClientRect().height}px`);
+  }
+  else if(currentAnsNum != e.target.value-1){
+    console.log("change");
+    btns[currentAnsNum].classList.remove("active");
+    currentAnsNum = e.target.value-1;
+    btns[currentAnsNum].classList.add("active");
+    bodys[currentAnsNum].classList.add("active");
+    bodys[currentAnsNum].style.setProperty("--max-height", `${bodys[currentAnsNum].firstElementChild.getBoundingClientRect().height}px`);
+  }
+  else{
+    console.log("same");
+    btns[currentAnsNum].classList.toggle("active");
+    bodys[currentAnsNum].classList.toggle("active");
+    bodys[currentAnsNum].style.setProperty("--max-height", `${bodys[currentAnsNum].firstElementChild.getBoundingClientRect().height}px`);
+  }
 }
